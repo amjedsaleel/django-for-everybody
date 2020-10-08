741371649f55f6049b37e399abbf91b6ee6db24b
Amjed Saleel K <amjedsaleel@gmail.com>
2020-10-08 20:10:27 +0530

    Django Features and Libraries week 1 assignment

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
-    'sample'
+    'django.contrib.staticfiles',   
+    'sample',
+    'hello'
 ]
 
 MIDDLEWARE = [
