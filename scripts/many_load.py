a4d632638aa9cd3f0c0ab49b279c5cfec45f0df6
Amjed Saleel K <amjedsaleel@gmail.com>
2020-10-22 17:43:01 +0530

    script created for upload data from csv to unesco model

--- scripts/many_load.py
+++ scripts/many_load.py
@@ -1,29 +1,67 @@
 import csv
-from many.models import Person, Course, Membership
+from unesco.models import Category, States, Region, Iso, Site
 
 
 def run():
-    csv_file = open('many/load.csv')
-    read_csv = csv.reader(csv_file)
+    csv_file = open('unesco/whc-sites-2018-clean.csv')
+    csv_read = csv.reader(csv_file)
 
-    # Person.objects.all().delete()
-    # Course.objects.all().delete()
-    # Membership.objects.all().delete()
+    Category.objects.all().delete()
+    States.objects.all().delete()
+    Region.objects.all().delete()
+    Iso.objects.all().delete()
+    Site.objects.all().delete()
 
     # Format
-    # email,role,course
-    # jane@tsugi.org,I,Python
-    # ed@tsugi.org,L,Python
+    # name	description	justification	year	longitude	latitude	area_hectares	category	states	region	iso
+    count = 1
+    for row in csv_read:
+        # print(row)
 
-    for row in read_csv:
-        print(row)
+        category, created = Category.objects.get_or_create(name=row[7])
+        states, created = States.objects.get_or_create(name=row[8])
+        region, created = Region.objects.get_or_create(name=row[9])
+        iso, created = Iso.objects.get_or_create(name=row[10])
 
-        p, created = Person.objects.get_or_create(email=row[0])
-        c, created = Course.objects.get_or_create(title=row[2])
+        name = row[0]
+        description = row[1]
+        justification = row[2]
 
-        r = Membership.LEARNER
-        if row[1] == 'I' :
-            r = Membership.INSTRUCTOR
+        try:
+            year = int(row[3])
+        except:
+            year = None
 
-        membership = Membership(role=r, person=p, course=c)
-        membership.save()
+        try:
+            longitude = float(row[4])
+        except:
+            longitude = None
+
+        try:
+            latitude = float(row[5])
+        except:
+            latitude = None
+
+        try:
+            area_hectares = float(row[6])
+        except:
+            area_hectares = None
+
+        print('Inserting data to Site model: ', count)
+        site = Site.objects.create(
+            name=name,
+            description=description,
+            justification=justification,
+            year=year,
+            longitude=longitude,
+            latitude=latitude,
+            area_hectares=area_hectares,
+            category=category,
+            states=states,
+            region=region,
+            iso=iso,
+        )
+        site.save()
+        count += 1
+
+    print("Finished")
