80e65b8faaeb207912e4e298b5794d96cc8f9de2
Amjed Saleel K <amjedsaleel@gmail.com>
2020-10-24 21:37:39 +0530

    pics completed

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.conf import settings
 
 # Create your models here.
+
+
+class Pic(models.Model):
+    title = models.CharField(
+            max_length=200,
+            validators=[MinLengthValidator(2, 'Tittle must be more than 2 characters')]
+    )
+    text = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    # Picture
+    picture = models.BinaryField(null=True, blank=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True, blank=True, help_text='The MIMEType of the file')
+
+    def __str__(self):
+        return self.title
+
