362f71fc79b59caeaff171576ea7f6eec3c110c0
Amjed Saleel K <amjedsaleel@gmail.com>
2020-10-25 14:17:41 +0530

    forum create view

--- forums/urls.py
+++ forums/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -6,5 +6,6 @@ app_name = 'forums'
 
 urlpatterns = [
     path('', views.ForumListView.as_view(), name='all'),
-    path('forum/<int:pk>', views.ForumDetailView.as_view(), name='forum_detail')
+    path('forum/<int:pk>', views.ForumDetailView.as_view(), name='forum_detail'),
+    path('create', views.ForumCreateView.as_view(success_url=reverse_lazy('forums:all')), name='forum_create')
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,6 +1,8 @@
+from django.db.transaction import commit
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from forums.models import Forum, Comment
 
@@ -17,3 +19,15 @@ class ForumDetailView(DetailView):
     model = Forum
     template_name = 'forums/detail.html'
     # context_object_name = 'detail'
+
+
+class ForumCreateView(LoginRequiredMixin, CreateView):
+    model = Forum
+    fields = ['title', 'text']
+    template_name = 'forums/form.html'
+
+    def form_valid(self, form):
+        object = form.save(commit=False)
+        object.owner = self.request.user
+        object.save()
+        return super(ForumCreateView, self).form_valid(form)
